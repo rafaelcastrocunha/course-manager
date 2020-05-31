53aa35341a8c93d6c40a922364020267f9d22a60
rafaelcastrocunha <rafaelcastrocunha@gmail.com>
2020-05-31 10:42:29 -0300

    Projeto finalizado

---
+++ src/app/shared/component/star/star.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { StarComponent } from './star.component';
+@NgModule({
+  declarations: [StarComponent],
+  exports: [StarComponent],
+})
+export class StarModule {}
